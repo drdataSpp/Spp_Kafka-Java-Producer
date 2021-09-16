d15bb57cb62b4c90ae6aed941e6e39e3f33fff2d
SOORYA PRAKASH PARTHIBAN <sooryaprakml@gmail.com>
2021-09-16 23:14:35 +1200

    Initial commit

---
+++ src/main/java/SampleProducer.java
@@ -0,0 +1,24 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SampleProducer {
+
+    public SampleProducer(){
+
+        Properties properties = new Properties();
+
+        properties.put("bootstrap.servers", "127.0.0.1:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        ProducerRecord producerRecord = new ProducerRecord("channel", "name", "Message sent to consumer!");
+
+        KafkaProducer kafkaProducer = new KafkaProducer(properties);
+
+        kafkaProducer.send(producerRecord);
+        kafkaProducer.close();
+    }
+
+}
